6245215f369f6c8b18f5b6788ff812d67b282ed3
Eric-Soto <48608238+Eric-Soto@users.noreply.github.com>
2020-05-19 18:42:23 -0700

    Add files via upload

---
+++ Rocket.js
@@ -0,0 +1,46 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var massKg = 0;
+        for (var i = 0; i < items.length; i++) {
+            massKg += items[i].massKg;
+        }
+        return massKg;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        var FullMassKg = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return FullMassKg;
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
